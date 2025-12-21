0d9bc38785cac5f5640e922d07136b43aa396452
unknown <souravsharma7194@gmail.com>
2025-12-21 11:53:13 +0530

    a indian tourism website

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className ="">
+      <Navbar />
+
+      <section className="min-h-screen">
+        <h1>Namaste</h1>
+      </section>
+
+      <section className="min-h-screen "></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen "></section>
+
+      <section className="min-h-screen"></section>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
